e616ee9f654d2a8dd8a9e6f62edd4ad664bb5fe1
DengXinbo <mymail33333@163.com>
2022-06-08 00:30:00 +0800

    fix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,12 +1,14 @@
 import * as d3 from 'd3';
 import { industryType, IndustryType, IndustryTypeNames } from '../types';
 
-const industryColor = d3.schemeOranges;
+const industryColor = d3.schemeSet1;
+
+const industryColorScale = d3.scaleOrdinal(industryType, industryColor);
 
 export function getIndustryNames(values: IndustryType[] = []) {
   return values?.map((d) => IndustryTypeNames[d]);
 }
 
 export function getIndustryColor(val: IndustryType): string {
-  return d3.scaleOrdinal().domain(industryType).range(industryColor)(val) as string;
+  return industryColorScale(val);
 }
