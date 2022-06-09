778d9ddcd6747078982b46515583e49a25778641
DengXinbo <mymail33333@163.com>
2022-06-10 01:21:45 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import 'virtual:windi.css'
-import "antd/dist/antd.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import 'virtual:windi.css';
+import 'antd/dist/antd.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
