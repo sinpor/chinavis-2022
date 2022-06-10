5c56e98216cfd296097dfcbef1c823bfbcde10af
DengXinbo <mymail33333@163.com>
2022-06-11 03:42:57 +0800

    update force

--- src/pages/home/components/Force.tsx
+++ src/pages/home/components/Force.tsx
@@ -15,6 +15,7 @@ import { ILinkData, INodeData, NodeTypeNames, LinkTypeNames } from '../../../typ
 import { observer } from 'mobx-react';
 import { StoreContext } from '../../../store';
 import { request } from '../../../utils/request/request';
+import { reaction } from 'mobx';
 
 const linkTypes = [
   'r_cert',
@@ -52,17 +53,27 @@ const nodeColorScale = d3.scaleOrdinal(nodeTypes, colors);
 const linkColorScale = d3.scaleOrdinal(linkTypes, linkColors);
 
 export const Force: React.FC = observer(() => {
-  const { currentData, updateSelectedNodes } = useContext(StoreContext);
+  const store = useContext(StoreContext);
+
+  const { currentData, selectedNodes, updateSelectedNodes } = store;
+
+  const selectedNodesRef = useRef<number[] | null>([]);
+
+  selectedNodesRef.current = selectedNodes;
 
   const containerRef = useRef<HTMLDivElement>(null);
 
   const svg = useRef<Selection<BaseType, any, any, any> | null>();
+
+  const canvasContext = useRef<CanvasRenderingContext2D | null>();
 
   const simulation = useRef<Simulation<INode, ILink> | null>(null);
 
   const forceNode = useRef<ForceManyBody<INode>>();
 
   const forceLink = useRef<ForceLink<INode, ILink>>();
+
+  const nodeSizeScale = useRef(d3.scaleLinear());
 
   const [box, setBox] = useState({ width: 0, height: 0 });
 
@@ -94,6 +105,10 @@ export const Force: React.FC = observer(() => {
       .alphaDecay(0.01);
     //   .velocityDecay(0.3);
 
+    nodeSizeScale.current = nodeSizeScale.current
+      .domain(d3.extent(nodes.map((d) => d.originData.weight)) as number[])
+      .range([3, 8]);
+
     return { forceNode, forceLink, simulation };
   }, []);
 
@@ -111,37 +126,73 @@ export const Force: React.FC = observer(() => {
 
       const nodeG = globalG?.append('g');
 
-      const dragRect = globalG
-        ?.append('rect')
-        .attr('x', -width / 2)
-        .attr('y', -height / 2)
-        .attr('width', width)
-        .attr('height', height)
-        .attr('fill', 'transparent');
+      //   const dragRect = globalG
+      //     ?.append('rect')
+      //     .attr('x', -width / 2)
+      //     .attr('y', -height / 2)
+      //     .attr('width', width)
+      //     .attr('height', height)
+      //     .attr('fill', 'transparent');
 
       const canvas = containerRef.current?.querySelector('canvas');
-      const context = canvas?.getContext('2d');
+      const context = canvasContext.current || canvas?.getContext('2d');
+      canvasContext.current = context as CanvasRenderingContext2D;
       (canvas as HTMLCanvasElement).width = width;
       (canvas as HTMLCanvasElement).height = height;
 
       //   const width = canvas.width;
       //   const height = canvas.height;
 
-      dragRect
-        ?.call(
+      const selectedG = globalG?.append('g').classed('selectedG', true);
+
+      let selectedCircle: Selection<SVGCircleElement, INode, SVGGElement, any>;
+
+      globalG
+        ?.append('g')
+        .classed('brushG', true)
+        .attr('transform', `translate(${-width / 2}, ${-height / 2})`)
+        .call(
           d3
-            .drag()
-            //   .container(dragRect?.node() as SVGRectElement)
-            .subject(dragsubject)
-            .on('start', dragstarted)
-            .on('drag', dragged)
-            .on('end', dragended) as any
+            .brush()
+            .filter(() => {
+              const node = simulation.current?.find(d3.event.offsetX, d3.event.offsetY, 10) as INode;
+              return !node;
+            })
+            .on('start', () => {
+              simulation.current?.stop();
+            })
+            .on('brush', () => {
+              //   console.log(d3.event);
+              //   console.log(d3.event);
+            })
+            .on('end', () => {
+              const selection = d3.event.selection;
+              if (!selection) {
+                simulation.current?.alphaTarget(0.5).restart();
+                updateSelectedNodes([]);
+                return;
+              }
+
+              const useData = nodes
+                .filter(
+                  (d) =>
+                    (d.x as number) >= selection[0][0] - width / 2 &&
+                    (d.x as number) <= selection[1][0] - width / 2 &&
+                    (d.y as number) >= selection[0][1] - height / 2 &&
+                    (d.y as number) <= selection[1][1] - height / 2
+                )
+                .map((d) => d.originData.id);
+
+              updateSelectedNodes(useData as number[]);
+            })
         )
         .on('mousemove', () => {
           const { offsetX, offsetY } = d3.event;
           const data = simulation.current?.find(offsetX - width / 2, offsetY - height / 2, 5);
 
           if (!data) {
+            overlay?.attr('cursor', 'crosshair');
+
             clearTimeout(timer);
             timer = setTimeout(() => {
               setPopoverData((prev) => {
@@ -150,6 +201,8 @@ export const Force: React.FC = observer(() => {
             }, 50);
             return;
           }
+
+          overlay?.attr('cursor', 'default');
 
           setPopoverData({
             x: offsetX,
@@ -163,18 +216,28 @@ export const Force: React.FC = observer(() => {
           const data = simulation.current?.find(offsetX - width / 2, offsetY - height / 2, 5);
 
           if (data) {
-			console.log(data.originData.id as number);
             updateSelectedNodes([data.originData.id as number]);
           }
         });
 
+      const overlay = globalG?.select('g.brushG rect.overlay');
+
+      overlay?.call(
+        d3.drag().subject(dragsubject).on('start', dragstarted).on('drag', dragged).on('end', dragended) as any
+      );
+
+      const coreData = nodes.filter((d) => d.originData.isCore);
+
+      //   核心节点
       const coreNode = nodeG
-        ?.append('use')
+        ?.selectAll('use')
+        .data(coreData)
+        .join('use')
         .attr('xlink:href', '#star')
         .attr('width', 20)
         .attr('height', 20)
         .attr('transform', 'translate(-10, -10)')
-        .classed('hidden', true);
+        .attr('fill', (d) => nodeColorScale(d.originData.label));
 
       function ticked() {
         context?.clearRect(0, 0, width, height);
@@ -185,10 +248,14 @@ export const Force: React.FC = observer(() => {
         nodes.forEach(drawNode);
 
         context?.restore();
+
+        coreNode?.attr('x', (d) => d?.x || 0).attr('y', (d) => d?.y || 0);
+
+        selectedCircle?.style('cx', (d) => d.x || 0).style('cy', (d) => d.y || 0);
       }
 
       function dragsubject(): INode {
-        return simulation.current?.find(d3.event.x, d3.event.y) as INode;
+        return simulation.current?.find(d3.event.x - width / 2, d3.event.y - height / 2, 10) as INode;
       }
 
       function dragstarted() {
@@ -219,25 +286,53 @@ export const Force: React.FC = observer(() => {
       }
 
       function drawNode(d: INode) {
-        //   核心节点
-        if (d.originData.isCore) {
-          coreNode
-            ?.attr('fill', nodeColorScale(d.originData.label))
-            .attr('x', d?.x || 0)
-            .attr('y', d?.y || 0)
-            .classed('hidden', false);
-        }
-
         context?.beginPath();
         context?.save();
         (context as CanvasRenderingContext2D).fillStyle = d.originData.isCore
           ? ' rgba(0,0,0,0)'
           : nodeColorScale(d.originData.label);
+
+        // (context as CanvasRenderingContext2D).strokeStyle = '#f00';
         context?.moveTo((d?.x || 0) + 3, d?.y || 0);
-        context?.arc(d?.x || 0, d?.y || 0, d.originData.isCore ? 10 : 3, 0, 2 * Math.PI);
+        context?.arc(
+          d?.x || 0,
+          d?.y || 0,
+          d.originData.isCore ? 10 : (nodeSizeScale.current(d.originData.weight) as number),
+          0,
+          2 * Math.PI
+        );
         context?.fill();
+        // if (selectedNodesRef.current?.includes(d.originData.id as number)) context?.stroke();
         context?.restore();
       }
+
+      const clear = reaction(
+        () => store.selectedNodes,
+        (val) => {
+          const selecteds = nodes.filter((d) => val?.includes(d.originData.id as number));
+
+          simulation.current?.alphaTarget(0.3).restart;
+
+          selectedCircle = selectedG
+            ?.html('')
+            .selectAll('circle')
+            .data(selecteds)
+            .join('circle')
+            .attr('fill', 'none')
+            .attr('stroke', (d) => nodeColorScale(d.originData.label))
+            .attr('stroke-width', 2)
+            .attr('cx', (d) => d?.x || null)
+            .attr('cy', (d) => d?.y || null)
+            .style('r', (d) => (nodeSizeScale.current?.(d.originData.weight) as number) + 2) as any;
+        },
+        { fireImmediately: true }
+      );
+
+      return () => {
+        clear();
+        simulation.current?.on('tick', null);
+        context?.clearRect(0, 0, width, height);
+      };
     },
     [box]
   );
@@ -253,7 +348,7 @@ export const Force: React.FC = observer(() => {
   useEffect(() => {
     if (box.width && currentData.nodes) {
       //   request('/mock/community_1.json', { baseURL: '' }).then((res) => {
-      const { links, nodes } = currentData;
+      const { nodes, links } = currentData;
       // const { nodes, relations: links } = res.data;
 
       if (box.width) {
@@ -275,10 +370,12 @@ export const Force: React.FC = observer(() => {
 
         forceNode.current.strength(nodeStrength);
 
-        initChart(useNodes, useLinks);
+        const clear = initChart(useNodes, useLinks);
 
         setNodes(useNodes);
         setLinks(useLinks);
+
+        return clear;
       }
       //   });
     }
@@ -317,9 +414,9 @@ export const Force: React.FC = observer(() => {
       </div>
       <div className="p-4 bottom-0 left-0 absolute">
         <div className="font-medium mb-4 text-gray-500">节点图例</div>
-        <div className="grid gap-y-2">
+        <div className=" grid gap-y-2">
           {nodeTypes.map((n) => (
-            <div key={n} className="flex items-center">
+            <div key={n} className="flex text-xs items-center">
               <span
                 className=" rounded-1 h-10px mr-2 w-10px inline-block"
                 style={{ backgroundColor: nodeColorScale(n) }}
@@ -327,7 +424,7 @@ export const Force: React.FC = observer(() => {
               <span>{(NodeTypeNames as any)[n]}</span>
             </div>
           ))}
-          <div className="flex items-center">
+          <div className="flex text-xs items-center">
             <span className=" rounded-1  mr-2 text-red-400 text-10px inline-block">
               <svg viewBox="0 0 1024 1024" width="1em" height="1em">
                 <use xlinkHref="#star" fill="currentColor"></use>
@@ -339,9 +436,9 @@ export const Force: React.FC = observer(() => {
       </div>
       <div className="text-right p-4 right-0 bottom-0 absolute">
         <div className="font-medium mb-4 text-gray-500">连线图例</div>
-        <div className="grid gap-y-2">
+        <div className="grid text-2xs gap-y-2">
           {linkTypes.map((l) => (
-            <div key={l} className="flex items-center justify-between">
+            <div key={l} className="flex text-xs items-center justify-between">
               <span>{(LinkTypeNames as any)[l]}</span>
               <span className=" h-3px ml-2 w-30px inline-block" style={{ backgroundColor: linkColorScale(l) }}></span>
             </div>
@@ -364,6 +461,11 @@ export const Force: React.FC = observer(() => {
               strokeWidth="0.5px"
               d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z"
             />
+          </symbol>
+          <symbol id="wave" viewBox="0 0 1024 1024">
+            <circle className="rp1" cx="100" cy="100" r="35" stroke="red" strokeWidth="2" fill="transparent" />
+            <circle className="rp2" cx="100" cy="100" r="45" stroke="red" strokeWidth="2" fill="transparent" />
+            <circle className="rp3" cx="100" cy="100" r="55" stroke="red" strokeWidth="2" fill="transparent" />
           </symbol>
         </defs>
       </svg>
